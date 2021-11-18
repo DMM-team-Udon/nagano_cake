d476cee0bd07d4b3624e3cbf9a1f355b6922afa9
T6776 <ec2-user@ip-172-31-24-5.ec2.internal>
2021-11-18 08:02:00 +0000

    [Add] cart_items

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,13 @@ class Product < ApplicationRecord
   attachment :image
   
   validates :name, :introduction, :price, :is_active
+
+  has_many :cart_items, dependent: :destroy
+  has_many :order, through: :order_detail
+  
+  ## 消費税を求めるメソッド
+  def with_tax_price
+    (price * 1.1).floor
+  end
+  
 end
