82abd610594e3c095447f4cf643d3bb1b746478b
T6776 <t930324@gmail.com>
2021-11-20 03:23:01 +0000

    [Add] 注文機能

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,17 @@
 class Product < ApplicationRecord
   belongs_to :genre
-  
+
   attachment :image
-  
+
 
   validates :name, :introduction, :price, presence: true
-  
+
   has_many :cart_items, dependent: :destroy
   has_many :order, through: :order_detail
-  
+
   ## 消費税を求めるメソッド
   def with_tax_price
     (self.price * 1.1).floor
   end
-  
+
 end
